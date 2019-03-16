825ad54189edd0145f4231dba03f429eaf47eae7
CORP\jrichter <jan-peter.richter@capgemini.com>
2019-03-16 22:30:52 +0100

    Initial commit of the templated devon4j project - including a quick&dirty .gitignore

---
+++ java/cashplanner2/core/src/main/java/eu/jprichter/cashplanner2/general/dataaccess/api/dao/BinaryObjectRepository.java
@@ -0,0 +1,11 @@
+package eu.jprichter.cashplanner2.general.dataaccess.api.dao;
+
+import com.devonfw.module.jpa.dataaccess.api.data.DefaultRepository;
+import eu.jprichter.cashplanner2.general.dataaccess.api.BinaryObjectEntity;
+
+/**
+ * {@link DefaultRepository} for {@link BinaryObjectEntity}.
+ */
+public interface BinaryObjectRepository extends DefaultRepository<BinaryObjectEntity> {
+
+}
---
+++ java/cashplanner2/core/src/test/java/eu/jprichter/cashplanner2/general/common/base/test/ApplicationComponentTest.java
@@ -0,0 +1,22 @@
+package eu.jprichter.cashplanner2.general.common.base.test;
+
+import com.devonfw.module.test.common.base.ComponentTest;
+
+import eu.jprichter.cashplanner2.SpringBootApp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+/**
+ * Abstract base class for {@link ComponentTest}s of this application.
+ */
+@SpringBootTest(classes = { SpringBootApp.class }, webEnvironment = WebEnvironment.NONE)
+public abstract class ApplicationComponentTest extends ComponentTest {
+
+  @Override
+  protected void doTearDown() {
+    super.doTearDown();
+    TestUtil.logout();
+  }
+
+}
