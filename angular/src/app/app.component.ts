d1c7e049bb744dbc76c726e1286d26481b7a730e
CORP\jrichter <jan-peter.richter@capgemini.com>
2019-12-30 23:00:43 +0100

    blind commit of all changes in order to have them saved and backed up - development will be stopped on this experimental project and re-started from scratch with devonfw 3.2.X

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+// see https://teradata.github.io/covalent/#/docs/icons
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cashplanner2';
+  title = 'Cashplanner2';
+
+  constructor(
+    private _iconRegistry: MatIconRegistry,
+    private _domSanitizer: DomSanitizer
+  ) {
+    this._iconRegistry.addSvgIconInNamespace(
+      'assets',
+      'euro',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-euro_symbol-24px.svg')
+    );
+  }
 }
